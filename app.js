14bcba65f7b961a6d62716dfd099c203a586cdb8
Nathan Griffith <ngriffith29@student.egcc.edu>
2018-07-31 16:20:43 -0400

    lab is finsihed

---
+++ app.js
@@ -0,0 +1,26 @@
+let fruits = ['apple', 'tomato', 'banana'];
+
+function printFruits() {
+    let favFruit = fruits[2];
+    console.log(fruits[0]);
+    printFavFruit();
+    function printFavFruit() {
+        console.log(favFruit);
+    }
+  
+    
+}3
+name()
+function name(){
+    console.log("hello nathan")
+}
+
+
+printFruits();
+
+
+
+let alertf = function (){
+    alert("HELLO WORLD!!!! (YES I PICKED THE MOST BASIC LINE EVER)")
+}
+alertf()
